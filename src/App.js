3afe99898d6adb82d799df3674646779904692de
Ashwani Kumar Maurya <69212778+ashwani-eng@users.noreply.github.com>
2023-09-23 09:11:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <>
+   
+  <Navbar title= 'Incedo' />
+  
+  
+  {/* <div className="container"><Textform heading="Type Your Text"/>
+    </div> */}
+  </>
+  );
+}
+
+export default App;
