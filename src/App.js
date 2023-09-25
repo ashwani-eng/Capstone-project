c95d2001debe958e495b0fb7a32815129669542b
Ashwani Kumar Maurya <69212778+ashwani-eng@users.noreply.github.com>
2023-09-25 08:58:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+import './App.css';
+import Dashboard from './components/Dashboard'
+import Sidebar from './components/Sidebar'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Employee from './Employee'
+import Home from './components/Home'
+import AddEmployee from './AddEmployee'
+
+
+function App() {
+  return (
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Sidebar />}></Route>
+    <Route path="/Dashboard" element={<Dashboard/>}></Route>
+    <Route path="/Employee" element={<Employee/>}></Route>
+    <Route path="/Home" element={<Home/>}></Route>
+    <Route path="/create" element={<AddEmployee/>}></Route>
+    
+    
+    
+
+  </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Employee.js
@@ -0,0 +1,29 @@
+
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function Employee() {
+  return (
+    <div className='px-5 py-3'>
+      <div className='d-flex justify-content-center mt-2'>
+        <h3>Participants List</h3>
+      </div>
+      <Link to="/create" className='btn btn-success'>Submit NewIdea</Link>
+      <div className='mt-3'>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Idea Name</th>
+              <th>Email</th>
+              <th>Description</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+        
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default Employee
